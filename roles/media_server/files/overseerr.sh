15406d3041db3814ce279e397ee159eb24bb066d
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2022-11-21 01:06:51 -0300

    WIP rc.d update.

--- roles/media_server/files/overseerr.sh
+++ roles/media_server/files/overseerr.sh
@@ -10,14 +10,17 @@ name="overseerr"
 desc="Overseerr daemon"
 rcvar="overseerr_enable"
 
-overseerr_chdir="/usr/local/share/overseerr"
-overseerr_user="overseerr"
+overseerr_chdir="/usr/local/${name}"
+overseerr_user="${name}"
 overseerr_command="/usr/local/bin/yarn"
 overseerr_args="--cwd ${overseerr_chdir} start"
 
-pidfile="/var/run/${name}.pid"
+pidprefix="/var/run/${name}"
+pidfile="${pidprefix}/${name}.pid"
 command="/usr/sbin/daemon"
-command_args="-P ${pidfile} -R 10 ${overseerr_command} ${overseerr_args}"
+command_args="-P ${pidfile} -R 10 -f -u ${overseerr_user} ${overseerr_command} ${overseerr_args}"
+
+mkdir -m 777 -p $pidprefix
 
 load_rc_config $name
 : ${overseerr_enable:=no}
